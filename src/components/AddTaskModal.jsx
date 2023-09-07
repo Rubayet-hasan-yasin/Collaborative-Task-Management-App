94849d629142e0ecd3ce41f9cb2303bbe0311f9b
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-09-07 20:31:13 +0600

    add task form added

---
+++ src/components/AddTaskModal.jsx
@@ -0,0 +1,118 @@
+import { useForm } from 'react-hook-form';
+import NormalModal from './Modal/NormalModal';
+
+
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+
+    const { handleSubmit, register, reset } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+
+        onCancel()
+    }
+
+    const onCancel = ()=>{
+        reset();
+        setIsOpen(false);
+    }
+
+    return (
+        <NormalModal
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+        >
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='flex gap-3 flex-col'>
+                    <label htmlFor="title" className='mb-2'>Title</label>
+                    <input
+                    className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                        type="text"
+                        id='title'
+                        {...register('name')}
+
+                    />
+
+                </div>
+
+                <div className='flex flex-col mb-5'>
+                    <label htmlFor="title" className='mb-2'>Description</label>
+                    <textarea 
+                    className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                    type="text"
+                    id='description'
+                    {...register('description')}
+                    ></textarea>
+
+                </div>
+                <div className='flex flex-col mb-5'>
+                    <label htmlFor="title" className='mb-2'>Deadline</label>
+                    < input
+                    className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                    type="date"
+                    id='date'
+                    {...register('date')}
+                    />
+
+                </div>
+
+                <div className='flex flex-col mb-5'>
+                    <label htmlFor="title" className='mb-2'>Assign to</label>
+                    <select
+                    className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                    {...register('assignedTo')}
+                    >
+                        <option value="Jahangir Alam">Jahangir Alam</option>
+                        <option value="Hiroshi Tanaka">Hiroshi Tanaka</option>
+                        <option value="Mohammad Rahman">Mohammad Rahman</option>
+                        <option value="Kamal Ahmed">Kamal Ahmed</option>
+                        <option value="Ahmed Khan">Ahmed Khan</option>
+                        <option value="Jahangir Alam">Jahangir Alam</option>
+                        <option value="Harun Mia">Harun Mia</option>
+                        <option value="Arif Hussain">Arif Hussain</option>
+                        <option value="Shahidul Islam">Shahidul Islam</option>
+                        <option value="Nasir Uddin">Nasir Uddin</option>
+                        <option value="Hasan Ali">Hasan Ali</option>
+                        <option value="Aminul Haque">Aminul Haque</option>
+                        <option value="Mahbub Alam">Mahbub Alam</option>
+                    </select>
+
+                </div>
+
+                
+
+                <div className='flex flex-col mb-5'>
+                    <label htmlFor="title" className='mb-2'>Priority</label>
+                    
+                    <select
+                    className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                     id="priority"
+                     {...register('priority')}
+                     >
+                        <option defaultValue value="high">High</option>
+                        <option value="medium">Medium</option>
+                        <option value="low">Low</option>
+                     </select>
+
+                </div>
+
+
+                <div className='flex gap-3 justify-end'>
+                <button
+                onClick={onCancel}
+                type='button' 
+                className='rounded-xl h-10 px-4 grid place-content-center font-semibold  transition-all text-white bg-primary'
+                >Cancel</button>
+
+                <button type='submit' className='rounded-xl h-10 px-4 grid place-content-center font-semibold  transition-all bg-red-500 text-white'>Submit</button>
+                </div>
+
+            </form>
+
+
+        </NormalModal>
+
+    );
+};
+
+export default AddTaskModal;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import { BsPlus } from "react-icons/bs";
+import AddTaskModal from "../components/AddTaskModal";
+
 
 const Home = () => {
-    return (
-        <div>
-          <h1>Home</h1>  
-        </div>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log(isOpen);
+
+
+  return (
+    <div className="container mx-auto mt-10">
+      <hr />
+
+      <p onClick={()=>setIsOpen(!isOpen)} className="flex items-center gap-2 group/add hover:text-red-700 mt-2 w-fit cursor-pointer">
+        <BsPlus className="text-2xl group-hover/add:bg-red-700 rounded-full group-hover/add:text-white" />
+        Add task
+      </p>
+      <AddTaskModal isOpen={isOpen} setIsOpen={setIsOpen}/>
+    </div>
+  );
 };
 
 export default Home;
